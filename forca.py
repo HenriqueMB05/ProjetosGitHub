f7c0b741715f92d7b88c27113744266e1c24cc35
Henrique <matosh38@gmail.com>
2024-07-15 21:33:47 -0300

    Corrigindo alguns problemas

--- forca.py
+++ forca.py
@@ -1,14 +1,16 @@
 from random import choice, randint
+
 # Banco de palavras
 dic = {
-    "Linguagens": ["Python", "Java", "JavaScript", "PHP", "lua", "ecmaScript", "TypeScript","C","C++", "C#", "Assembly"],
+    "Linguagens": ["Python", "Java", "JavaScript", "PHP", "lua", "ecmaScript", "TypeScript","C","Cpp", "Csharp", "Assembly"],
     "Comida": ["Tacaca", "Vatapa", "Acai", "Feijoada", "moqueca","Ensopado", "buchada","Strogonoff","Bolo", "Cocada"],
     "Trabalhos": ["chapeiro","Policial","Acougueiro", "Empresario", "Mecanico","Padeiro", "Confeiteiro"]
 }
+
 # Sorteando a chave do dicionario de forma separada, para conseguir usar no len de sua lista e também como a dica
 key = choice(list(dic.keys()))
 palavra = dic[key][randint(0,len(dic[key])-1)]
-print(palavra)
+
 letras_usuario = []
 
 chances = 4
@@ -30,13 +32,17 @@ while True:
     print(f"Você tem {chances} chances")
 
     tentativa = input("Escolha uma letra para adivinhar: ")
-    letras_usuario.append(tentativa.lower())
 
     
 
     # Esse bloco funciona para a finalização do jogo, caso as chances se zerem ou caso a variavel ganhou mantenha o seu status de True será finalizado.
     if tentativa.lower() not in palavra.lower():
-        chances -= 1
+        if tentativa.lower()  not in letras_usuario:
+            chances -= 1
+    if tentativa not in letras_usuario:
+        letras_usuario.append(tentativa.lower())
+    else:
+        print("Você já digitou essa letra")
     ganhou = True
     for letra in palavra:
         if letra.lower() not in letras_usuario:
